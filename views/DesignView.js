9b519717783aa756053e46e62f513d6acb73a199
DanielRasho <ray22933@uvg.edu.gt>
2024-09-04 07:19:02 -0600

    feat: finish TEXT section on Design view

--- views/DesignView.js
+++ views/DesignView.js
@@ -1,3 +1,8 @@
+import { MAIN_CARD } from "../stores/CardStore.js";
+import { TextBoxModel } from "../models/TextBoxModel.js";
+import { DragabbleElement } from "../models/DragabbleElement.js";
+import { DEFAULT_FONT_STYLES } from "../models/TextBoxModel.js";
+
 class DesignView extends HTMLElement {
     
     constructor() {
@@ -6,12 +11,47 @@ class DesignView extends HTMLElement {
 
     connectedCallback() {
         this.render()
+        this.recreateTextInputs()
+        this.addTextFieldListener()
+    }
+
+    recreateTextInputs(){
+        let textBoxes = MAIN_CARD.textBoxes
+        const sectionTexts = document.getElementById('section-texts-list')
+        sectionTexts.innerHTML = '' // Clear data
+        
+        textBoxes.forEach((textBoxModel, index) => {
+            const textInput = document.createElement('text-input')
+            
+            textInput.setModel(textBoxModel)
+            textInput.setAttribute('text-box-id', index)
+
+            // HERE PASS THE RENDER FUNCTION
+            textInput.parentRender = () => this.recreateTextInputs();
+
+            sectionTexts.append(textInput)
+        })
+    }
+
+    addTextFieldListener(){
+        const addTextFieldButton = document.getElementById('add-text-field-btn')
+        addTextFieldButton.addEventListener('click', () => {
+            MAIN_CARD.addTextBox(new TextBoxModel(
+                new DragabbleElement(
+                    'card-previewer',
+                    10, 10,
+                    60, 30,
+                    2,
+                ),
+                "",
+                DEFAULT_FONT_STYLES
+            ))
+            this.recreateTextInputs()
+        })
     }
     
     render() {
         this.innerHTML = /*html*/`
-        <link rel="stylesheet" href="./styles/main.css">
-        <link rel="stylesheet" href="./styles/designView.css">
         <div class='design-view'>
             <div class='design-view_controllers'>
                 <ul>
@@ -31,15 +71,20 @@ class DesignView extends HTMLElement {
                     </li>
                     <li>
                         <input 
-                            type="radio" 
+                            type="radio"
                             name="accordion" 
                             id="design-controls_text">
                         <label for="design-controls_text">
                             <span class="section-header">Text</span>
                             <span class="retro-font">02</span>
                         </label>
-                        <div class="section-content">
-                            content
+                        <div id="section-texts" class="section-content">
+                            <div id="section-texts-list">
+                            </div>
+                            <secondary-button 
+                                text="New field" 
+                                after-icon="fa-solid fa-plus" 
+                                button-id="add-text-field-btn">
                         </div>
                     </li>
                     <li>
@@ -58,7 +103,7 @@ class DesignView extends HTMLElement {
                 </ul>
             </div>
             <div class='design-view_preview'>
-                <div class='card-previewer'>
+                <div id='card-previewer' class='card-previewer'>
                 </div>
                 <div> 
                     <main-button 
